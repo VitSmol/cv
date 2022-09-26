bc463104f56ffc488f20b13cc0788886cfc1068e
VitSmol <smolitskiy@mail.ru>
2022-09-26 15:04:46 +0300

    fix: fix some styles and markup

--- online-zoo/pages/test/app.js
+++
@@ -1,9 +0,0 @@
-let oRangeWrap = document.querySelector('.range-wrap');
-let oRangeText = oRangeWrap.querySelector('.range-text');
-let aRangeVals = ['Today', '2 days', '3 days', '4 days', '5 days', '6 days', '7 days'];
-document.querySelector('[type="range"]').addEventListener('input', function() {
-  let nVal = this.value - 1;
-  oRangeText.textContent = aRangeVals[nVal];
-  oRangeWrap.style.setProperty('--left', `${(100 / 6) * nVal}%`);
-  oRangeText.style.transform = `translatex(-${(100 / 6) * nVal}%)`;
-});
