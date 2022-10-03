095b10c255b2b19734d77a4f1b8c84a3aa309d96
VitSmol <smolitskiy@mail.ru>
2022-10-03 14:35:50 +0300

    feat: add all. TODO: check mistakes

--- online-zoo/assets/styles/app.js
+++ online-zoo/assets/styles/app.js
@@ -1,4 +1,18 @@
+const radio1000 = document.getElementById('three')
 const radio100 = document.getElementById('six')
-window.onchange(() => {
-    console.log();
-})
+
+const checkResolution = () => {
+    if (window.outerWidth <= 740) {
+        radio100.setAttribute('checked', 'true')
+        radio1000.removeAttribute('checked')
+    } else {
+        const radio1000 = document.getElementById('three')
+        radio100.removeAttribute('checked')
+        radio1000.setAttribute('checked', 'true')
+    }
+}
+
+checkResolution()
+window.onresize = () => {
+    checkResolution()
+}
