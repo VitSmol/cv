fe09de6a9fc70144ff56d99e93c42924bf7ecdb0
VitSmol <smolitskiy@mail.ru>
2022-11-14 21:58:11 +0300

    feat: add next questions

--- songbird/js/index.js
+++ songbird/js/index.js
@@ -5,13 +5,40 @@ import * as game from "./game.js";
 const descriptionBlock = document.querySelector(`.hidden-description`);
 const startDescription = document.querySelector(`.start-text`)
 const listOfQuestions = document.querySelector(`.lists`);
+const scoreCount = document.getElementById(`score-count`)
+
+const defaultSrc = `assets/img/bird.jpg`
+
+const questionImage = document.getElementById(`question-img`)
+const questionName = document.querySelector(`.question-name`)
+
+const image = document.getElementById(`bird-image`);
+const name = document.getElementById(`bird-name`);
+const nameLat = document.getElementById(`bird-lat`);
+const text = document.querySelector(`.description-text`);
+
+const nexLevelBtn = document.querySelector(`.next-level`)
+const birdsListItems = document.querySelectorAll(`.birds__list__item`)
+nexLevelBtn.disabled = true
+
+//todo реализовать: 1.таймер времени
+//todo реализовать: 2.общая продолжительность времени
+//todo реализовать: 3.клик по кнопке с обнулением и переходом к следующему разделу
+//todo реализовать: 4.звук правильного/не правильного ответа
+
 
 let questions;
 
 let currentIndex = 0;
-let score = 0;
 let showQuestions = false
-let currentArray
+let currentArray;
+
+const activeChapter = (ind, arr) => {
+  arr.forEach(el => {
+    el.classList.remove(`active`)
+  })
+  arr[ind].classList.add(`active`)
+}
 
 const random = (min, max) => {
   let rand = min - 0.5 + Math.random() * (max - min + 1)
@@ -22,7 +49,7 @@ const createQuestionsList = (arr, index) => {
   currentArray = [...arr[index]]
   let rightQuestion = random(0, arr.length - 1)
 
-  currentArray.forEach((el,ind) => {
+  currentArray.forEach((el, ind) => {
     const li = document.createElement(`li`);
     li.classList.add(`questions__list__item`);
     li.setAttribute(`data-matrix-id`, `${el.id}`)
@@ -30,9 +57,8 @@ const createQuestionsList = (arr, index) => {
     div.classList.add(`point`)
     li.append(div, el.name)
     listOfQuestions.append(li)
-    
+
     if (ind === rightQuestion) {
-      // todo при загрузке изменяется src но ползунок становится на середину на середине
       el.isTrue = true
       game.audio.src = el.audio
       game.audio.addEventListener('loadeddata', () => {
@@ -41,7 +67,7 @@ const createQuestionsList = (arr, index) => {
     }
   });
   questions = document.querySelectorAll(`.questions__list__item`);
-  console.log(questions);
+  // console.log(questions);
 }
 
 createQuestionsList(birdsData, currentIndex)
@@ -52,7 +78,13 @@ questions.forEach(el => {
   })
 })
 
+// Переменная определеят решен ли текущий таск
+let resolve = false
+let score = 0;
+let step = 0;
+
 function clickByQuestions(el) {
+
   if (!showQuestions) {
     showQuestions = true
     descriptionBlock.classList.toggle(`hidden`)
@@ -62,27 +94,65 @@ function clickByQuestions(el) {
     game.playDescription.classList.add(`active`)
     game.pauseDescription.classList.remove(`active`)
   }
-  const image = document.getElementById(`bird-image`);
-  const name = document.getElementById(`bird-name`);
-  const nameLat = document.getElementById(`bird-lat`);
-  const text = document.querySelector(`.description-text`);
+
   let currentId = el.dataset.matrixId
   let obj = currentArray.find(el => {
     return el.id == currentId
   })
-  console.log(obj);
-  // game.soundDescription.style.background = `linear-gradient(to right, rgb(0, 188, 140) ${game.soundDescription.value * 100}%, rgb(61, 133, 140) ${game.soundDescription.value * 100}%, rgb(115, 115, 115) ${game.soundDescription.value * 100}%, rgb(115, 115, 115) ${game.soundDescription.value * 100}%)`
-    image.src = obj.image;
-    name.innerHTML = obj.name;
-    nameLat.innerHTML = obj.species;
-    text.innerHTML = `${obj.description}`
-    game.audioDescription.src = obj.audio
-    game.audioDescription.addEventListener('loadeddata', () => {
-      game.progressDescription.value = 0
-    })
-    game.progressDescription.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) 0%, rgb(115, 115, 115) 0%, rgb(115, 115, 115) 0%)`
-    
-  if (obj.isTrue) {
-    console.log(`You win`);
+  image.src = obj.image;
+  name.innerHTML = obj.name;
+  nameLat.innerHTML = obj.species;
+  text.innerHTML = `${obj.description}`
+  game.audioDescription.src = obj.audio
+  game.audioDescription.addEventListener('loadeddata', () => {
+    game.progressDescription.value = 0
+  })
+  game.progressDescription.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) 0%, rgb(115, 115, 115) 0%, rgb(115, 115, 115) 0%)`
+  if (!obj.isTrue && !resolve) {
+    el.firstChild.classList.add(`wrong`)
+    step++
+  }
+  if (obj.isTrue && !resolve) {
+    el.firstChild.classList.add(`win`)
+    score += 5 - step
+    resolve = true
+    scoreCount.innerHTML = score
+    questionImage.src = obj.image
+    questionName.innerHTML = obj.name
+    nexLevelBtn.disabled = false
   }
 }
+
+const toNext = () => {
+  game.audio.addEventListener('loadeddata', () => {
+    game.progress.value = 0
+  })
+  game.audioDescription.addEventListener('loadeddata', () => {
+    game.progressDescription.value = 0
+  })
+  game.progressDescription.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) 0%, rgb(115, 115, 115) 0%, rgb(115, 115, 115) 0%)`
+  game.progress.style.background = `linear-gradient(to right, rgb(0, 188, 140) 0%, rgb(61, 133, 140) 0%, rgb(115, 115, 115) 0%, rgb(115, 115, 115) 0%)`
+ 
+  descriptionBlock.classList.toggle(`hidden`)
+  startDescription.classList.toggle(`hidden`)
+  showQuestions = false
+  resolve = false
+  step = 0;
+  currentIndex++
+  listOfQuestions.innerHTML = ``
+  questionImage.src = defaultSrc
+  questionName.innerHTML = `******`
+  activeChapter(currentIndex, birdsListItems)
+  createQuestionsList(birdsData, currentIndex)
+}
+
+nexLevelBtn.addEventListener(`click`, () => {
+  toNext()
+  
+  questions.forEach(el => {
+    el.addEventListener(`click`, () => {
+      clickByQuestions(el)
+    })
+  })
+  nexLevelBtn.disabled = true
+})
